9153bc08d744bf5de81179fa747ea5c889f89607
212213 <marselmubaraksin231@mail.ru>
2024-06-05 10:52:26 +0300

    добавлена аудидорожка

--- Player/MainWindow.xaml.cs
+++ Player/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Player
 {
@@ -22,78 +23,99 @@ namespace Player
     /// </summary>
     public partial class MainWindow : Window
     {
-  
-            private IWavePlayer wavePlayer;
-            private AudioFileReader audioFileReader;
-            private List<string> musicFiles;
-            private int currentTrackIndex;
 
-            public MainWindow()
+        private IWavePlayer wavePlayer;
+        private AudioFileReader audioFileReader;
+        private List<string> musicFiles;
+        private int currentTrackIndex;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            wavePlayer = new WaveOutEvent();
+            musicFiles = new List<string>();
+            currentTrackIndex = -1;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                InitializeComponent();
-                wavePlayer = new WaveOutEvent();
-                musicFiles = new List<string>();
-                currentTrackIndex = -1;
-            }
+                Multiselect = true,
+                Filter = "Music Files|*.mp3;*.wav;*.wma"
+            };
 
-            private void LoadButton_Click(object sender, RoutedEventArgs e)
+            if (openFileDialog.ShowDialog() == true)
             {
-                OpenFileDialog openFileDialog = new OpenFileDialog
-                {
-                    Multiselect = true,
-                    Filter = "Music Files|*.mp3;*.wav;*.wma"
-                };
-
-                if (openFileDialog.ShowDialog() == true)
-                {
-                    musicFiles.AddRange(openFileDialog.FileNames);
-                    currentTrackIndex = 0;
-                    LoadCurrentTrack();
-                }
-            }
-
-            private void PlayButton_Click(object sender, RoutedEventArgs e)
-            {
-                wavePlayer.Play();
-            }
-
-            private void PauseButton_Click(object sender, RoutedEventArgs e)
-            {
-                wavePlayer.Pause();
-            }
-
-            private void NextButton_Click(object sender, RoutedEventArgs e)
-            {
-                if (musicFiles.Count > 0)
-                {
-                    currentTrackIndex = (currentTrackIndex + 1) % musicFiles.Count;
-                    LoadCurrentTrack();
-                    wavePlayer.Play();
-                }
-            }
-
-            private void LoadCurrentTrack()
-            {
-                if (currentTrackIndex >= 0 && currentTrackIndex < musicFiles.Count)
-                {
-                    audioFileReader?.Dispose();
-                    audioFileReader = new AudioFileReader(musicFiles[currentTrackIndex]);
-                    wavePlayer.Init(audioFileReader);
-                    CurrentSongText.Text = $"Playing: {System.IO.Path.GetFileName(musicFiles[currentTrackIndex])}";
-                }
-            }
-
-            protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
-            {
-                wavePlayer.Dispose();
-                audioFileReader?.Dispose();
-                base.OnClosing(e);
-            }
-
-            public void Dispose()
-            {
-                throw new NotImplementedException();
+                musicFiles.AddRange(openFileDialog.FileNames);
+                currentTrackIndex = 0;
+                LoadCurrentTrack();
             }
         }
-    }
 
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            wavePlayer.Play();
+            timer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            wavePlayer.Pause();
+            timer.Stop();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (musicFiles.Count > 0)
+            {
+                currentTrackIndex = (currentTrackIndex + 1) % musicFiles.Count;
+                LoadCurrentTrack();
+                wavePlayer.Play();
+            }
+        }
+
+        private void LoadCurrentTrack()
+        {
+            if (currentTrackIndex >= 0 && currentTrackIndex < musicFiles.Count)
+            {
+                audioFileReader?.Dispose();
+                audioFileReader = new AudioFileReader(musicFiles[currentTrackIndex]);
+                wavePlayer.Init(audioFileReader);
+                CurrentSongText.Text = $"Playing: {System.IO.Path.GetFileName(musicFiles[currentTrackIndex])}";
+                PositionSlider.Maximum = audioFileReader.TotalTime.TotalSeconds;
+                TotalTimeText.Text = audioFileReader.TotalTime.ToString(@"mm\:ss");
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (audioFileReader != null)
+            {
+                PositionSlider.Value = audioFileReader.CurrentTime.TotalSeconds;
+                CurrentTimeText.Text = audioFileReader.CurrentTime.ToString(@"mm\:ss");
+            }
+        }
+
+        private void PositionSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (audioFileReader != null && Math.Abs(audioFileReader.CurrentTime.TotalSeconds - e.NewValue) > 1)
+            {
+                audioFileReader.CurrentTime = TimeSpan.FromSeconds(e.NewValue);
+            }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            wavePlayer.Dispose();
+            audioFileReader?.Dispose();
+            base.OnClosing(e);
+        }
+    }
+}
